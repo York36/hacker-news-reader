cad222912dc820493d93cda6960927e813cc556a
York36 <joshuayork33@gmail.com>
2020-06-22 10:31:14 -0700

    Updates

---
+++ precache-manifest.84a8dd079a50c8e57f97b26ccf262e68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618db33a79ba6352501eecac543b40a9",
+    "url": "/hacker-news-reader/index.html"
+  },
+  {
+    "revision": "52d69b619b8641919de7",
+    "url": "/hacker-news-reader/static/js/2.106b5c6d.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-reader/static/js/2.106b5c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b675d5b79cb4c025a73",
+    "url": "/hacker-news-reader/static/js/main.9aaf0c05.chunk.js"
+  },
+  {
+    "revision": "15cab9a75f106f9b64b9",
+    "url": "/hacker-news-reader/static/js/runtime-main.20d9bca5.js"
+  }
+]);
